61f68240ee5a97eb74984a5ec8c5e3b1d4ec29f7
alik-laha <aliklaha@outlook.com>
2024-04-29 12:36:19 +0530

    fix the earning view

--- client/src/componenet/home/earningCard.tsx
+++ client/src/componenet/home/earningCard.tsx
@@ -1,23 +1,32 @@
 import { Card, Text, Metric } from "@tremor/react";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Context from "../context/context";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Earnings } from "../../type/globaleType";
 
 const EarningCard = () => {
-    const { setEarningData, totalEarning } = useContext(Context)
-
+    const { setEarningData } = useContext(Context)
+    const [totalEarning, setTotalEarning] = useState<number>(0)
     const navigate = useNavigate();
     const fetchData = () => {
         axios.get("/api/expense/getallearnings")
             .then((res): void => {
                 setEarningData(res.data.earnings)
+                calculataEarning(res.data.earnings)
+
             }
             ).catch((err) => {
                 console.log(err)
             }
             )
     }
+    const calculataEarning = (data: Earnings[]) => {
+        data.map((item: Earnings) => {
+            setTotalEarning((prev: number) => prev + item.amount)
+        })
+    }
+
     useEffect(() => {
         fetchData()
     }, [])
