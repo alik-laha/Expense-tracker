394abda533472be285e1791b1ea3c9aab7ad41fa
alik-laha <aliklaha@outlook.com>
2024-05-10 20:00:49 +0530

    join or create page ready

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,22 +1,43 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// Load environment variables from .env file
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     sourcemap: true,
+//   },
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: process.env.API_URL, // Proxy target URL from environment variable
+//         changeOrigin: true,
+//         // Optionally, you can use rewrite to remove the '/api' prefix from the request path
+//         // rewrite: (path) => path.replace(/^\/api/, '')
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+
 import dotenv from 'dotenv';
 
-dotenv.config(); // Load environment variables from .env file
-
+dotenv.config();
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
   server: {
     proxy: {
+      // Proxy /api requests to http://localhost:8000
       '/api': {
-        target: process.env.API_URL, // Proxy target URL from environment variable
+        target: process.env.API_URL,
         changeOrigin: true,
-        // Optionally, you can use rewrite to remove the '/api' prefix from the request path
-        // rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/api/, '/api')
       },
-    },
-  },
+      // Proxy /images requests to another server
+      '/images': {
+        target: 'http://another-server.com',
+        changeOrigin: true
+      }
+    }
+  }
 });
