347cf705b22462ff2b7307de0af941d4fa8e49cc
alik-laha <aliklaha@outlook.com>
2024-05-10 21:36:26 +0530

    join or create page ready

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,32 +1,13 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// Load environment variables from .env file
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     sourcemap: true,
-//   },
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: process.env.API_URL, // Proxy target URL from environment variable
-//         changeOrigin: true,
-//         // Optionally, you can use rewrite to remove the '/api' prefix from the request path
-//         // rewrite: (path) => path.replace(/^\/api/, '')
-//       },
-//     },
-//   },
-// });
-
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "https://expense-tracker-46x2.onrender.com/",
-        changeOrigin: true
+        target: "https://expense-tracker-46x2.onrender.com", // Your backend server URL
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
       }
     }
   }
