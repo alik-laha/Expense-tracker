9fa4a98dd6d784ae93683777bec56fc00f23cb0d
alik-laha <aliklaha@outlook.com>
2024-05-11 11:23:38 +0530

    join or create page ready

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
-
+import dotenv from 'dotenv';
+dotenv.config();
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "http://localhost:3000/", // Your backend server URL
+        target: process.env.API_URL, // Your backend server URL
         changeOrigin: true,
         secure: true
       }
     }
   }
+
 });
